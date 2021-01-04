6019cc2787bff3358d13c76edb4fc17aaae66ef5
Matt Kellock <matt@kellock.com.au>
2021-01-04 11:00:13 +1100

    Adding POCs as submodules

--- Install.ps1
+++ Install.ps1
@@ -5,7 +5,7 @@ minikube delete
 minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --kubernetes-version=v1.19.0 --memory=4g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
 
 # Deploy the monitoring stack foundational services
-./Monitoring/Install.ps1
+./MonitoringPOC/Install.ps1
 
 # Deploy RabbitMQ
-./RabbitMQ/Install.ps1
+./RabbitMQKubernetesPOC/Install.ps1
