e1fab51c2cc5a8b2e2a405c0d71f521d80645ef7
Matt Kellock <matt@kellock.com.au>
2021-01-05 11:01:02 +1100

    Revised version of the install script

--- Install.ps1
+++ Install.ps1
@@ -14,7 +14,7 @@ Write-Output $breaker
 Write-Output "Creating MiniKube cluster"
 Write-Output $breaker
 minikube start --vm-driver hyperv --hyperv-virtual-switch "Default Switch" --kubernetes-version=v1.19.0 --memory=4g --bootstrapper=kubeadm --extra-config=kubelet.authentication-token-webhook=true --extra-config=kubelet.authorization-mode=Webhook --extra-config=scheduler.address=0.0.0.0 --extra-config=controller-manager.address=0.0.0.0
-
+minikube addons disable metrics-server
 
 # Deploy the monitoring stack foundational services
 Write-Output $breaker
